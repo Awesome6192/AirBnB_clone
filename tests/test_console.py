aafe31d652d8739809efdd9107fc862a8b7caf73
Awesome6192 <christianchibuike14@gmail.com>
2024-05-16 17:27:38 +0100

    good job

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,65 +1,50 @@
 #!/usr/bin/python3
 """
-Unit tests for console using Mock module from python standard library
-Checks console for capturing stdout into a StringIO object
+Test suits for the base model
 """
 
 import os
-import sys
+import re
+import json
+import uuid
 import unittest
-from unittest.mock import create_autospec, patch
-from io import StringIO
-from console import HBNBCommand
-from models import storage
+from time import sleep
+from datetime import datetime
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
-class TestConsole(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
-    Unittest for the console model
+    Tests attributes of the base model
     """
 
     def setUp(self):
-        """Redirecting stdin and stdout"""
-        self.mock_stdin = create_autospec(sys.stdin)
-        self.mock_stdout = create_autospec(sys.stdout)
-        self.err = ["** class name missing **",
-                "** class doesn't exist **",
-                "** instance id missing **",
-                "** no instance found **",
-                ]
-
-        self.cls = ["BaseModel",
-                "User",
-                "State",
-                "City",
-                "Place",
-                "Amenity",
-                "Review"]
-
-    def create(self, server=None):
         """
-        Redirects stdin and stdout to the mock module
+        Classes needed for testing
         """
-        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+        pass
 
-    def last_write(self, nr=None):
-        """Returns last n output lines"""
-        if nr is None:
-            return self.mock_stdout.write.call_args[0][0]
-        return "".join(map(lambda c: c[0][0],
-            self.mock_stdout.write.call_args_list[-nr:]))
+    def test_basic(self):
+        """
+        Tests basic imputs for the BaseModel class
+        """
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number],
+                ["ALX", 89])
 
-    def test_quit(self):
-        """Quit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("quit"))
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
+
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
 
 
 if __name__ == '__main__':
